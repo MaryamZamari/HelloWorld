9b3f1f15df80693f7da787916e0e6a2f224a5350
MaryamZamari <zaamari.maryam@gmail.com>
2023-11-02 13:44:39 +0100

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        String text= "Hello World!";
+        System.out.println(text);
+       
+    }
+}
